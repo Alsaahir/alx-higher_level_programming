627954b3b6ddbe959d6b8681084b12f338204fb2
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-06-02 23:30:17 -0700

    print args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+
+def main(*argv):
+    i = 0
+    arg_count = len(sys.argv) - 1
+    if arg_count == 1:
+        print("{:d} arguments:".format(arg_count))
+    elif arg_count == 0:
+        print("{:d} arguments.".format(arg_count))
+    else:
+        print("{:d} arguments:".format(arg_count))
+    for args in sys.argv:
+        if (i != 0):
+            print("{}: {}".format(i, args))
+        i += 1
+
+
+if __name__ == "__main__":
+    main()
