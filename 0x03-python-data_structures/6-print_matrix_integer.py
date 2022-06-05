b1d84afcfc590b0313b2e05eec0c21db202992ed
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-06-04 22:27:36 -0700

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+    for x in range(matrix_len):
+        for y in range(len(matrix[x])):
+            print('{:d}'.format(matrix[x][y]), end='')
+            if y is not (len(matrix[x]) - 1):
+                print(end=" ")
+        print("")
